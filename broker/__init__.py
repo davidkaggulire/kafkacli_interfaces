870eb2315e0662abe8e4e79190cb924e08fe5c88
davidkaggulire <dkaggulire@gmail.com>
2022-02-02 16:34:50 +0300

    add inmemory broker

--- broker/__init__.py
+++ broker/__init__.py
@@ -3,3 +3,4 @@
 from .broker_interface import IMessageBroker
 from .kafka_broker import KafkaBroker
 from .kafka_broker_listener import KafkaBrokerListener
+from .inmemory_broker import InMemoryBroker
