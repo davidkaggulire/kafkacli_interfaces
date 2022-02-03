e9b800a3982d33246359bb941d661f7c55b7f146
davidkaggulire <dkaggulire@gmail.com>
2022-02-03 15:03:06 +0300

    inmemory broker

--- broker/inmemory_broker.py
+++ broker/inmemory_broker.py
@@ -1,10 +1,6 @@
 # inmemory_broker.py
 
-import concurrent.futures
 import logging
-import queue
-import threading
-import time
 
 from .broker_interface import IMessageBroker
 
@@ -12,58 +8,29 @@ from .broker_interface import IMessageBroker
 class InMemoryBroker(IMessageBroker):
 
     def __init__(self):
-        self.event = threading.Event()
-        self.pipeline = queue.Queue(maxsize=2)
+        self.pipeline = []
 
     def sendMessages(self, args: dict) -> bool:
-        format = "%(asctime)s: %(message)s"
-        logging.basicConfig(format=format, level=logging.INFO,
-                            datefmt="%H:%M:%S")
-        logging.getLogger().setLevel(logging.DEBUG)
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:  
-            executor.submit(InMemoryBroker.producer, self.pipeline, self.event, args)
-
-            time.sleep(0.1)
-            # logging.info("Main: about to set event")
-            self.event.set()
+        self.producer(self.pipeline, args)
         return True
 
     def readMessages(self, args: dict) -> bool:
-        format = "%(asctime)s: %(message)s"
-        logging.basicConfig(format=format, level=logging.INFO,
-                            datefmt="%H:%M:%S")
-        logging.getLogger().setLevel(logging.DEBUG)
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(InMemoryBroker.consumer, self.pipeline, self.event)
-
-            time.sleep(0.1)
-            logging.info("Main: about to set event")
-            self.event.set()
+        self.consumer(self.pipeline)
         return True
 
-  
-    def producer(queue, event, args):
-        """Pretend we're getting a number from the network."""
-        while not event.is_set():
-            message = args['message']
-            queue.put(message)
-            logging.info(f"Producer got message: {message}")
-            
-            # if message == "stop session":
-            #     break
-            break
+    def producer(self, queue, args):
+        """getting message from the network."""  
+        message = args['message']
+        # saving message to broker
+        queue.append(message)
+        print(queue)
+        print(f"Producer got message: *{message}* from network")
+        print("Producer... Exiting")
         
-        logging.info("Producer received event. Exiting")
-        
-
-    def consumer(queue, event):
-        """Pretend we're saving a number in the database."""
-        while not event.is_set() or not queue.empty():
-            message = queue.get()
-            logging.info(
-                f"Consumer received and storing message: {message}"
-            )
-
-        logging.info("Consumer received event. Exiting")
+    def consumer(self, queue):
+        """retrieving received message."""
+        message = queue.pop(0)
+        print(
+            f"Consumer received and storing message: {message}"
+        )
+        print("Consumer... Exiting")
