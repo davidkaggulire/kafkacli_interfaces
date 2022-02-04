968335a2de771407bd3bba36526a758f241b562f
davidkaggulire <dkaggulire@gmail.com>
2022-02-04 14:03:54 +0300

    add file to be tested by mocks

---
+++ broker/kafka_mock.py
@@ -0,0 +1,139 @@
+# kafka_broker.py
+
+from confluent_kafka import Producer, Consumer
+import argparse
+import sys
+from .broker_interface import IMessageBroker
+# from . import IMessageBroker
+
+
+class KafkaBrokerMock(IMessageBroker):
+
+    def parsed_args(self, args):
+        """
+        parse command line arguments needed for Kafka
+        """
+        parser = argparse.ArgumentParser(description="Send and receive messages using Kafka CLI")
+        parser.add_argument('command', choices=['send', 'receive'], help="Parse in either send or receive")
+        parser.add_argument('--channel', required=True, help="the channel or topic to send the message to")
+        parser.add_argument('--server', required=True, help="server connection")
+        parser.add_argument('--group', help="group to send messages to")
+        parser.add_argument('--from', choices=['start', 'latest'], default='start', help="choose which messages by start or latest")
+        args = parser.parse_args(args)
+        print(vars(args))
+        return vars(args)
+
+    def get_input():
+        data = input("Enter message to send or 'q' to quit: ")
+        if data == 'q':
+            sys.exit(0)
+        return data
+
+    def encode_func(args):
+        returned_data = args["message"]
+        return returned_data.encode('utf-8')
+
+    def delivery_report(err, msg):
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). """
+        if err:
+            return 'Message delivery failed: {}'.format(err)
+        else:
+            return 'Message delivered to {} [{}]'.format(msg.topic(), msg.partition())
+
+    def produce_method(args):
+        connection = args['server']
+        channel = args['channel']
+        group = args['group']
+        from_where = args['from']
+
+        print(f"You have decided to send to the channel: {channel}")
+        print(f"You are sending through the server: {connection}")
+        print(f"You are sending through the group: {group}")
+        print(f"You are sending through the group: {from_where}")
+
+        p = Producer({'bootstrap.servers': connection})
+
+        data = KafkaBrokerMock.encode_func(args)
+
+        return p.produce(channel, data, callback=KafkaBrokerMock.delivery_report)
+
+    def flush_method(args):
+        value = KafkaBrokerMock.produce_method(args)
+        return value.flush()
+
+    def sendMessages(self, args):
+        connection = args['server']
+        channel = args['channel']
+        group = args['group']
+        from_where = args['from']
+
+        print(f"You have decided to send to the channel: {channel}")
+        print(f"You are sending through the server: {connection}")
+        print(f"You are sending through the group: {group}")
+        print(f"You are sending through the group: {from_where}")
+
+        p = Producer({'bootstrap.servers': connection})
+
+        data = KafkaBrokerMock.encode_func(args)
+
+        p.produce(channel, data, callback=KafkaBrokerMock.delivery_report)
+        p.flush()
+        
+        return True
+
+    def consume_messages(args):
+        """
+        function to read messages
+        """
+        channel = args['channel']
+        server = args['server']
+        start_from = args['from']
+        group = args['group']
+
+        print(f"You are receiving from the channel: {channel}")
+        print(f"You are receiving from the {start_from}")
+        print(f"You are receiving through the server {server}")
+        print(f"You are part of the receiving group: {group}")
+
+        message_level = {
+            'start': 'beginning',
+            'latest': 'latest'
+        }
+        c = Consumer({
+            'bootstrap.servers': args['server'],
+            'group.id': args['group'],
+            'auto.offset.reset': message_level[args['from']],
+            # 'default.api.timeout.ms': 1000
+        })
+
+        return c
+
+    def readMessages(self, args):
+        c = KafkaBrokerMock.consume_messages(args)
+        c.subscribe([args['channel']])
+
+        try:
+            while True:
+                msg = c.poll(1.0)
+
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("Consumer error: {}".format(msg.error()))
+                    continue
+
+                var = msg.value().decode('utf-8')
+                print(f"Received message: {var}")
+
+                if var == "stopsession":
+                    print("stop message sent")
+                    break
+
+        except KeyboardInterrupt:
+            sys.stderr.write('%% Aborted by user\n')
+        finally:
+            c.close()
+            return True
+
+# hello = KafkaBroker()
