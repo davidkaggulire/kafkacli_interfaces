bc0de6dbff90fec16f97932aba25dcdac4f940df
davidkaggulire <dkaggulire@gmail.com>
2022-02-04 14:03:25 +0300

    testing with mocks on producer

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,5 +1,6 @@
 # tests.py
 
+from re import T
 import sys
 import os
 
@@ -8,14 +9,14 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import unittest
 from unittest.mock import patch
-from confluent_kafka import Consumer
-from broker.kafka_broker import KafkaBroker
+from confluent_kafka import Consumer, Producer
+from broker.kafka_mock import KafkaBrokerMock
 from unittest import mock
 
 
 class TestChatApp(unittest.TestCase):
     def setUp(self) -> None:
-        self.broker = KafkaBroker()
+        self.broker = KafkaBrokerMock()
         return self.broker
 
     def test_empty_parsed_args(self):
@@ -29,7 +30,7 @@ class TestChatApp(unittest.TestCase):
             ['send', '--channel', 'chat', '--server', 'localhost:9092'])
         self.assertIsInstance(parser, dict)
 
-    @patch('broker.kafka_broker.KafkaBroker')
+    @patch('broker.kafka_mock.KafkaBrokerMock')
     def test_parser_send(self, test_patch):
         """testing parsing command send"""
         test_patch.return_value = {
@@ -44,7 +45,7 @@ class TestChatApp(unittest.TestCase):
         self.assertIsInstance(parser, dict)
         self.assertEqual(parser, test_patch.return_value)
 
-    @patch('broker.kafka_broker.KafkaBroker')
+    @patch('broker.kafka_mock.KafkaBrokerMock')
     def test_parser_receive(self, test_patch):
         """testing parsing command receive"""
         test_patch.return_value = {
@@ -63,29 +64,15 @@ class TestChatApp(unittest.TestCase):
     def test_get_input_func(self):
         """test get_input function"""
         with patch('builtins.input', return_value='y'):
-            assert KafkaBroker.get_input() == 'y'
+            assert KafkaBrokerMock.get_input() == 'y'
         with patch('builtins.input', return_value='q'):
             self.assertRaises(SystemExit)
         with self.assertRaises(SystemExit) as cm:
             with patch('builtins.input', return_value='q'):
-                KafkaBroker.get_input()
+                KafkaBrokerMock.get_input()
         self.assertEqual(cm.exception.code, 0)
 
-    # @patch('broker.kafka_broker.KafkaBroker')
-    def test_send_messages(self):
-        """testing sending of message via kafka"""
-        args = {
-            'command': 'send',
-            'channel': 'chat',
-            'server': 'localhost:9092',
-            'group': "hello",
-            'from': 'start',
-            "message": "stopsession"
-        }
-        sent = self.broker.sendMessages(args)
-        self.assertEqual(sent, True)
-
-    # @patch('broker.kafka_broker.KafkaBroker')
+    # # @patch('broker.kafka_broker.KafkaBroker')
     def test_read_messages(self):
         """test reading message from Kafka"""
         data = {
@@ -107,7 +94,80 @@ class TestChatApp(unittest.TestCase):
         received = self.broker.readMessages(args)
         self.assertEqual(True, received)
 
-    @patch('broker.kafka_broker.KafkaBroker')
+    @patch('broker.kafka_mock.Producer')
+    def test_produce_method(self, mock_producer):
+        """testing sending of message via kafka"""
+        args = {
+            'command': 'send',
+            'channel': 'chat',
+            'server': 'localhost:9092',
+            'group': "hello",
+            'from': 'start',
+            "message": "Welcome to Uganda"
+        }
+        mock_producer.return_value = mock.Mock()
+        mock_producer.produce = None
+        response = KafkaBrokerMock.produce_method(args)
+        self.assertTrue(response, mock_producer)
+
+    @patch('broker.kafka_mock.Producer')
+    def test_flush_method(self, mock_producer):
+        """testing sending of message via kafka"""
+        args = {
+            'command': 'send',
+            'channel': 'chat',
+            'server': 'localhost:9092',
+            'group': "hello",
+            'from': 'start',
+            "message": "Welcome to Uganda"
+        }
+        # mock_producer.return_value = mock.Mock()
+        mock_producer.return_value = mock.Mock()
+        mock_producer.flush = False
+        response = KafkaBrokerMock.flush_method(args)
+        self.assertTrue(response, mock_producer)
+
+    @patch('broker.kafka_mock.Producer')
+    def test_send_messages(self, mock_producer):
+        """testing sending of message via kafka"""
+        args = {
+            'command': 'send',
+            'channel': 'chat',
+            'server': 'localhost:9092',
+            'group': "hello",
+            'from': 'start',
+            "message": "Welcome to Uganda"
+        }
+        mock_producer.return_value = mock.Mock()
+        mock_producer.flush = False
+        # response = KafkaBrokerMock.flush_method(args)
+        response = self.broker.sendMessages(args)
+        self.assertEqual(response, True)
+
+
+    # @patch('broker.kafka_broker.KafkaBroker')
+    # def test_read_messages(self):
+    #     """test reading message from Kafka"""
+    #     data = {
+    #         'command': 'send',
+    #         'channel': 'chat',
+    #         'server': 'localhost:9092',
+    #         'group': "hello",
+    #         'from': 'start',
+    #         "message": "stopsession"
+    #     }
+    #     args = {
+    #         'command': 'send',
+    #         'channel': 'chat',
+    #         'server': 'localhost:9092',
+    #         'group': "hello",
+    #         'from': 'start'
+    #     }
+    #     self.broker.sendMessages(data)
+    #     received = self.broker.readMessages(args)
+    #     self.assertEqual(True, received)
+
+    @patch('broker.kafka_mock.KafkaBrokerMock')
     def test_consume_messages(self, mock_consumer):
         args = {'command': 'receive', 'channel': 'chat',
                 'server': 'localhost:9092', 'group': 'hello', 'from': 'start'}
@@ -118,7 +178,7 @@ class TestChatApp(unittest.TestCase):
         }
         mock_consumer.return_value = Consumer(arg2)
 
-        result = KafkaBroker.consume_messages(args)
+        result = KafkaBrokerMock.consume_messages(args)
 
         self.assertIsInstance(mock_consumer.return_value, Consumer)
         self.assertIsInstance(result, Consumer)
@@ -132,13 +192,13 @@ class TestChatApp(unittest.TestCase):
             'from': 'start',
             "message": "stopsession"
         }
-        result = KafkaBroker.encode_func(args)
+        result = KafkaBrokerMock.encode_func(args)
         self.assertEqual(b'stopsession', result)
 
-    @patch('broker.kafka_broker.KafkaBroker')
+    @patch('broker.kafka_mock.KafkaBrokerMock')
     def test_delivery_report_error(self, mock_delivery):
         mock_delivery.return_value = mock.Mock("Message delivery failed: err")
-        result = KafkaBroker.delivery_report("err", "msg")
+        result = KafkaBrokerMock.delivery_report("err", "msg")
         self.assertEqual("Message delivery failed: err", result)
         self.assertFalse(mock_delivery.called)
 
