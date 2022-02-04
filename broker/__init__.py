52f050b3ff7e0bd9ce6e7602330d91aff290c286
davidkaggulire <dkaggulire@gmail.com>
2022-02-04 14:01:45 +0300

    Adding mock class

--- broker/__init__.py
+++ broker/__init__.py
@@ -4,3 +4,4 @@ from .broker_interface import IMessageBroker
 from .kafka_broker import KafkaBroker
 from .kafka_broker_listener import KafkaBrokerListener
 from .inmemory_broker import InMemoryBroker
+from .kafka_mock import KafkaBrokerMock
