58e6885c8e08e2144b780ff2e7c92d895c9a9895
davidkaggulire <dkaggulire@gmail.com>
2022-02-04 10:10:00 +0300

    implementing threading for class variable

---
+++ tests/test_class_variable.py
@@ -0,0 +1,80 @@
+# test_listener.py
+
+import sys
+import os
+import pytest
+import threading
+import logging
+import concurrent.futures
+import time
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from broker.kafka_broker_listener import KafkaBrokerListener
+
+
+@pytest.fixture
+def service(getBroker):
+    message_app = KafkaBrokerListener()
+    print(message_app)
+    return message_app
+
+
+@pytest.fixture
+def getBroker():
+    """return broker"""
+    return KafkaBrokerListener()
+
+
+def test_send_messages(service):
+    print(service)
+    data = {
+        'command': 'send',
+        'channel': 'stoplistener',
+        'server': 'localhost:9092',
+        'group': "hello",
+        'from': 'start',
+        "message": "welcome Uganda"
+    }
+    output = service.sendMessages(data)
+    assert output is True
+
+
+def test_read_messages(service):
+
+    data = {
+        'command': 'send',
+        'channel': 'stoplistener',
+        'server': 'localhost:9092',
+        'group': "hello",
+        'from': 'start',
+        "message": "welcome Uganda"
+    }
+    service.sendMessages(data)
+
+    data2 = {
+        'command': 'send',
+        'channel': 'stoplistener',
+        'server': 'localhost:9092',
+        'group': "hello",
+        'from': 'start',
+
+    }
+
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    
+    event = threading.Event()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(service.readMessages, data2)
+        assert True
+        time.sleep(1)
+        service.set_listener()
+        logging.info("Main: about to set event")
+        event.set()
+        logging.info(service.set_listener())
+
+    output = service.readMessages(data2)
+    assert output is True
